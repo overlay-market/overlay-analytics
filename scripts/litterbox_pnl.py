e6c65499e70d91c2630a5f27424b1987d05dd6e8
deepsp94 <anantsp94@gmail.com>
2023-02-16 21:45:17 +0530

    Function for token balances

--- scripts/litterbox_pnl.py
+++ scripts/litterbox_pnl.py
@@ -1,6 +1,11 @@
-import position_pnl as pp
+import scripts.position_pnl as pp
 
 
 def main():
     btc_pnl = pp.main('0x70cb456202e9ad25d3fdf1d0ac5d6b299a42dc99', 16002247)
     eth_pnl = pp.main('0x7f72986e190bbd1d02dac52b8dda82eea363d313', 16002247)
+
+    # Get list of all position builders
+    user_list = list(set(list(btc_pnl.user) + list(eth_pnl.user)))
+
+    # Get initial and final OVL balances
---
+++ scripts/state/balances.py
@@ -0,0 +1,8 @@
+from brownie import multicall
+
+
+def get_balances(users, token):
+    multicall(address='0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696')
+    with multicall:
+        bal = [token.balanceOf(user) for user in users]
+    return bal
