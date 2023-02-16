3ddc29436a2a8c4f357f8d0a81fbf3d47984fcf2
deepsp94 <anantsp94@gmail.com>
2023-02-16 21:14:35 +0530

    Change how inputs are called

--- scripts/pnl.py
+++ scripts/pnl.py
@@ -7,6 +7,10 @@ from scripts.events import build
 from scripts import utils
 
 
+ADDR = '0x70cb456202e9ad25d3fdf1d0ac5d6b299a42dc99'
+FROM_BLK = 16002247
+
+
 def active_pos_value(positions, state):
     multicall(address='0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696')
     with multicall:
@@ -15,35 +19,36 @@ def active_pos_value(positions, state):
     return pos_value
 
 
-def main(addr, from_block):
+def main():
     # Load contracts
-    market = utils.load_contract(addr)
+    market = utils.load_contract(ADDR)
     state = utils.load_contract('0x477122219aa1F76E190f480a85af97DE0A643320')
     token = utils.load_contract('0xdc77acc82cce1cc095cba197474cc06824ade6f7')
     print('Contracts loaded')
     # Get events dataframes
     build_df = eu.get_event_df(
         contract=market,
-        from_block=from_block,
+        from_block=FROM_BLK,
         event_type='Build'
     )
     unwind_df = eu.get_event_df(
         contract=market,
-        from_block=from_block,
+        from_block=FROM_BLK,
         event_type='Unwind'
     )
     liq_df = eu.get_event_df(
         contract=market,
-        from_block=from_block,
+        from_block=FROM_BLK,
         event_type='Liquidate'
     )
     trans_df = eu.get_event_df(
         contract=token,
-        from_block=from_block,
+        from_block=FROM_BLK,
         event_type='Transfer'
     )
     trans_df = eu.transfer_cols(trans_df)
     print('Dataframes built')
+    breakpoint()
 
     # Join build and transfers to get initial collateral from `value`
     build_df = build.join_build_trans(build_df, trans_df)
