5f8f03c4133440a0ae0aeeac9e35aadc91fdf4a3
deepsp94 <anantsp94@gmail.com>
2023-02-16 21:51:15 +0530

    Get current balances of users

--- scripts/litterbox_pnl.py
+++ scripts/litterbox_pnl.py
@@ -1,7 +1,13 @@
 import scripts.position_pnl as pp
+from scripts.state import balances as bal
+from scripts.utils import load_contract
 
 
 def main():
+    # Load OVL contract
+    ovl = load_contract('0xdc77acc82cce1cc095cba197474cc06824ade6f7')
+
+    # Get positions wise PnLs
     btc_pnl = pp.main('0x70cb456202e9ad25d3fdf1d0ac5d6b299a42dc99', 16002247)
     eth_pnl = pp.main('0x7f72986e190bbd1d02dac52b8dda82eea363d313', 16002247)
 
@@ -9,3 +15,4 @@ def main():
     user_list = list(set(list(btc_pnl.user) + list(eth_pnl.user)))
 
     # Get initial and final OVL balances
+    bals = bal.get_balances(user_list, ovl)
--- scripts/state/balances.py
+++ scripts/state/balances.py
@@ -1,7 +1,7 @@
 from brownie import multicall
 
 
-def get_balances(users, token):
+def get_current_balances(users, token):
     multicall(address='0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696')
     with multicall:
         bal = [token.balanceOf(user) for user in users]
