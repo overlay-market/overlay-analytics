cebf9d21da70a6117d1ff2c778099588f2a047e2
deepsp94 <anantsp94@gmail.com>
2023-02-17 17:24:04 +0530

    Get trade PnL per user

--- scripts/litterbox_pnl.py
+++ scripts/litterbox_pnl.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import scripts.position_pnl as pp
 from scripts.state import balances as bal
 from scripts.utils import load_contract
@@ -14,9 +15,21 @@ def main():
     eth_pnl = pp.main('0x7f72986e190bbd1d02dac52b8dda82eea363d313',
                       start_block)
 
+    # Get user wise PnLs
+    btc_pnl = btc_pnl.loc[:, ['user', 'total_pnl']]\
+        .groupby('user').sum().reset_index()
+    eth_pnl = eth_pnl.loc[:, ['user', 'total_pnl']]\
+        .groupby('user').sum().reset_index()
+    total_pnl = eth_pnl.merge(
+        btc_pnl, how='outer', on='user',
+        suffixes=('_eth', '_btc')
+    ).fillna(0, inplace=True)
+
     # Get list of all position builders
     user_list = list(set(list(btc_pnl.user) + list(eth_pnl.user)))
 
     # Get initial and final OVL balances
     curr_bals = bal.get_current_balances(user_list, ovl)
+    curr_bals = pd.DataFrame({'user': user_list, 'curr_balance': curr_bals})
     old_bals = bal.get_past_balances(user_list, ovl, start_block)
+    old_bals = pd.DataFrame({'user': user_list, 'old_balance': old_bals})
